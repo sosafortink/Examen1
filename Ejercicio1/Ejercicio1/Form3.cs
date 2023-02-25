016f9021f5390c988b0e6d5176a1cd3316265871
sosafortink <124389926+sosafortink@users.noreply.github.com>
2023-02-24 21:58:24 -0600

    Correccion de interes
    Correciones

---
+++ Ejercicio1/Ejercicio1/Form3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    listBox1.Items.Add("KarlaSosa");
+
+                }else if (i%3 == 0)
+                {
+                    listBox1.Items.Add("Karla");
+                }
+                else if (i%5==0)
+                {
+                    listBox1.Items.Add("Sosa");
+                }
+                else
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+        }
+    }
+}
